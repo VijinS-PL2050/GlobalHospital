b7bf63d7cc0718c215bbc8ca4b75c99179b57600
VijinS <vijin93@gmail.com>
2023-05-29 10:45:35 +0530

    my first commit

---
+++ src/main/java/com/faith/app/APP.java
@@ -0,0 +1,10 @@
+package com.faith.app;
+
+public class APP {
+
+	public static void main(String[] args) {
+		System.out.println("Demo Employee app V2023");
+
+	}
+
+}
---
+++ src/main/java/com/faith/model/Employee.java
@@ -0,0 +1,9 @@
+package com.faith.model;
+
+public class Employee {
+	
+	private int age;
+	private String name;
+	
+
+}
